ffc0f39bfa2fd555d6165b5fa0e5a5a6ea811bd2
Juan Diego Herranz <juand.herranz@gmail.com>
2024-12-06 15:28:06 +0100

    Playbook 3 correcto

--- scripts/add_vars.sh
+++ scripts/add_vars.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Nombre del archivo
 FILE="/home/jdherranz/git/TFG/scripts/vars.env"
-
-# Línea a buscar
 LINE_TO_REMOVE="NEW_NAME="
 
-# Verificar si la línea existe en el archivo
 if grep -E "$LINE_TO_REMOVE" "$FILE"; then
    grep -vE "$LINE_TO_REMOVE" "$FILE" > temp_file && mv temp_file "$FILE"
 else
-   echo "La línea '$LINE_TO_REMOVE' no existe. No se hará nada."
+   echo "La línea '$LINE_TO_REMOVE' no existe."
 fi
